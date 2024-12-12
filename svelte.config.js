31d42bd09431de86f836ed279a6f4cc794208154
pryority <matthewapryor@gmail.com>
2024-12-12 15:55:32 -0500

    ✨ Add protectedOrigins

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,9 @@ const config = {
       dynamic_origin: true, // This helps with Railway's proxy setup
       precompress: true, // Enable compression for better performance
     }),
+    csrf: {
+      protectedOrigins: ["https://packersly-production.up.railway.app"],
+    },
     env: {
       dir: ".",
       publicPrefix: "PUBLIC_",
