409fca6d465bee6ba58d49e7a5a7a8946bbe76f8
pryority <matthewapryor@gmail.com>
2024-12-12 17:02:22 -0500

    🚀 Deploy with csrf: true

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,6 @@ const config = {
       precompress: true, // Enable compression for better performance
       envPrefix: "",
     }),
-    csrf: {
-      checkOrigin: false, // Only disable this temporarily for testing
-    },
     env: {
       dir: ".",
       publicPrefix: "PUBLIC_",
