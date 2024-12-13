1401323934603cf72afba24e7ceb3ce30de81c18
pryority <matthewapryor@gmail.com>
2024-12-13 16:00:27 -0500

    ✨ Add qrCode table

---
+++ src/lib/server/db/schema/qrCode.ts
@@ -0,0 +1,27 @@
+import { pgTable, text, uuid, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import box from "./box";
+
+const qrCode = pgTable("qr_code", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  boxId: uuid("box_id")
+    .references(() => box.id, { onDelete: "cascade" })
+    .unique(), // Makes this a one-to-one relationship
+  code: text("code").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
+
+export const qrCodeRelations = relations(qrCode, ({ one }) => ({
+  box: one(box, {
+    fields: [qrCode.boxId],
+    references: [box.id],
+  }),
+}));
+
+export type QrCode = typeof qrCode.$inferSelect;
+export default qrCode;
