b0d518f60bddbf7fbe24293953e745b1f6a2fec4
pryority <matthewapryor@gmail.com>
2024-12-13 16:00:43 -0500

    🐛 Add qrCode relations

--- src/lib/server/db/schema/box.ts
+++ src/lib/server/db/schema/box.ts
@@ -2,13 +2,11 @@ import { pgTable, text, uuid, timestamp, boolean } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import room from "./room";
 import item from "./item";
+import qrCode from "./qrCode";
 
 const box = pgTable("box", {
   id: uuid("id").defaultRandom().primaryKey(),
-  roomId: uuid("room_id")
-    .notNull()
-    .references(() => room.id),
-  qrCode: text("qr_code").notNull().unique(),
+  roomId: uuid("room_id").references(() => room.id, { onDelete: "cascade" }),
   notes: text("notes"),
   accessToken: text("access_token").notNull().unique(),
   isPublic: boolean("is_public").notNull().default(false),
@@ -26,6 +24,10 @@ export const boxRelations = relations(box, ({ one, many }) => ({
     references: [room.id],
   }),
   items: many(item),
+  qrCode: one(qrCode, {
+    fields: [box.id],
+    references: [qrCode.boxId],
+  }),
 }));
 
 export type Box = typeof box.$inferSelect;
