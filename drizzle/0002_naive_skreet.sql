695d4c55da2252a8eff6095a5d4bb0e5653c33b6
pryority <matthewapryor@gmail.com>
2025-01-02 14:49:31 -0500

    🐛 Fix migration

--- drizzle/0002_naive_skreet.sql
+++ drizzle/0002_naive_skreet.sql
@@ -1,4 +1,2 @@
-ALTER TABLE "qr_code" ADD COLUMN "room_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "qr_code" ADD COLUMN "is_assigned" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "qr_code" ADD COLUMN "is_pre_generated" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "qr_code" ADD CONSTRAINT "qr_code_room_id_room_id_fk" FOREIGN KEY ("room_id") REFERENCES "public"."room"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "qr_code" ADD COLUMN "is_assigned" boolean DEFAULT false NOT NULL;
+ALTER TABLE "qr_code" ADD COLUMN "is_pre_generated" boolean DEFAULT false NOT NULL;
