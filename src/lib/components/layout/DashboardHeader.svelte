b031c7a3838a79ee97b0ec75a0e3225f9750bbef
pryority <matthewapryor@gmail.com>
2025-01-03 13:35:18 -0500

    💄 Stop truncate if just 'Dashboard' in breadcrumb

--- src/lib/components/layout/DashboardHeader.svelte
+++ src/lib/components/layout/DashboardHeader.svelte
@@ -138,9 +138,13 @@
       {#each breadcrumbs as crumb, i}
         <Breadcrumb.Item>
           {#if i === breadcrumbs.length - 1}
-            <Breadcrumb.Page class="truncate max-w-[16vw]"
-              >{crumb.label}</Breadcrumb.Page
+            <Breadcrumb.Page
+              class={breadcrumbs.length > 1
+                ? "truncate max-w-[16vw]"
+                : "truncate"}
             >
+              {crumb.label}
+            </Breadcrumb.Page>
           {:else}
             <Breadcrumb.Link
               href={crumb.href}
