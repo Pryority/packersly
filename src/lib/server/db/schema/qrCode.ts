6f1c924b791859022d5ff09086fb62c3b1287b3d
pryority <matthewapryor@gmail.com>
2025-02-01 13:12:49 -0500

    ✨ Add room key to QrCode type

--- src/lib/server/db/schema/qrCode.ts
+++ src/lib/server/db/schema/qrCode.ts
@@ -1,37 +1,37 @@
 import { pgTable, text, uuid, timestamp, boolean } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import box from "./box";
-import room from "./room";
+import room, { type Room } from "./room";
 
 const qrCode = pgTable("qr_code", {
-	id: uuid("id").defaultRandom().primaryKey(),
-	roomId: uuid("room_id")
-		.references(() => room.id, { onDelete: "cascade" })
-		.notNull(),
-	boxId: uuid("box_id")
-		.references(() => box.id, { onDelete: "cascade" })
-		.unique(), // Makes this a one-to-one relationship
-	url: text("url").notNull(),
-	isAssigned: boolean("is_assigned").default(false).notNull(),
-	isPreGenerated: boolean("is_pre_generated").default(false).notNull(),
-	createdAt: timestamp("created_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
-	updatedAt: timestamp("updated_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
+  id: uuid("id").defaultRandom().primaryKey(),
+  roomId: uuid("room_id")
+    .references(() => room.id, { onDelete: "cascade" })
+    .notNull(),
+  boxId: uuid("box_id")
+    .references(() => box.id, { onDelete: "cascade" })
+    .unique(), // Makes this a one-to-one relationship
+  url: text("url").notNull(),
+  isAssigned: boolean("is_assigned").default(false).notNull(),
+  isPreGenerated: boolean("is_pre_generated").default(false).notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
 });
 
 export const qrCodeRelations = relations(qrCode, ({ one }) => ({
-	box: one(box, {
-		fields: [qrCode.boxId],
-		references: [box.id],
-	}),
-	room: one(room, {
-		fields: [qrCode.roomId],
-		references: [room.id],
-	}),
+  box: one(box, {
+    fields: [qrCode.boxId],
+    references: [box.id],
+  }),
+  room: one(room, {
+    fields: [qrCode.roomId],
+    references: [room.id],
+  }),
 }));
 
-export type QrCode = typeof qrCode.$inferSelect;
+export type QrCode = typeof qrCode.$inferSelect & { room: Room };
 export default qrCode;
